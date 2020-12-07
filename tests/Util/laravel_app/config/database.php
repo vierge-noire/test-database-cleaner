a458a97dafb0963ec62cb9a2668f24b30e3e20b4
Juan Pablo Ramirez <pabloelcolombiano@gmail.com>
2020-12-07 13:13:19 +0100

    Port fixed for pgsql

--- tests/Util/laravel_app/config/database.php
+++ tests/Util/laravel_app/config/database.php
@@ -48,22 +48,18 @@ return [
         'default' => [
             'driver' => env('LARAVEL_DRIVER'),
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USER', 'forge'),
-            'password' => env('DB_PWD', ''),
+            'host' => env('DB_HOST'),
+            'port' => env('DB_PORT'),
+            'database' => env('DB_DATABASE'),
+            'username' => env('DB_USER'),
+            'password' => env('DB_PWD'),
             ConnectionManagerInterface::SNIFFER_CONFIG_KEY => TestUtil::getSnifferClassName(),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
+            'unix_socket' => env('DB_SOCKET'),
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
         ],
 
         'test_dummy' => [
