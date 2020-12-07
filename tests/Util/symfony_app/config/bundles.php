5fbcde3415986c3cf872b7f8ba7ced6b75500869
Juan Pablo Ramirez <pabloelcolombiano@gmail.com>
2020-12-07 13:04:39 +0100

    Symfony removed

--- tests/Util/symfony_app/config/bundles.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-//    \Doctrine\ORM\EntityManagerInterface::class => ['all' => true],
-//    \Doctrine\ORM\EntityManager::class => ['all' => true],
-];
